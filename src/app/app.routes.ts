4056d8c45bd1a60eaf33fccebe552c6634485e44
FABRICIO INGLEZ <fabricioinglez@gmail.com>
2025-04-18 16:05:03 -0300

    Aterações iniciais

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ListaComponent } from './pages/lista/lista.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: '', component: HomeComponent },
+    { path: 'lista-usuarios', component: ListaComponent },
+
+
+];
